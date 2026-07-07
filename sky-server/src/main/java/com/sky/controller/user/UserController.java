e5c5da57790b74898e9eca574d4d394ff6e6b497
Ke1logG <2251656765@qq.com>
2026-07-07 16:47:07 +0800

    微信登录功能

---
+++ sky-server/src/main/java/com/sky/controller/user/UserController.java
@@ -0,0 +1,51 @@
+package com.sky.controller.user;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sky.dto.UserLoginDTO;
+import com.sky.properties.JwtProperties;
+import com.sky.result.Result;
+import com.sky.service.UserService;
+import com.sky.utils.JwtUtil;
+import com.sky.vo.UserLoginVO;
+import com.sky.entity.User;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@RestController
+@RequestMapping("/user/user")
+@Api("tags = 用户登录")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private JwtProperties jwtProperties;
+
+
+    @PostMapping("/login")
+    @ApiOperation("登录模块")
+    public Result<UserLoginVO> login(UserLoginDTO userLoginDTO){
+        User user = userService.wxLogin(userLoginDTO);
+        //生成JWT令牌
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("userId", user.getId());
+        String token = JwtUtil.createJWT(jwtProperties.getUserSecretKey(), jwtProperties.getUserTtl(), claims);
+
+        UserLoginVO userLoginVO = UserLoginVO.builder()
+                .id(user.getId())
+                .openid(user.getOpenid())
+                .token(token)
+                .build();
+        return Result.success(userLoginVO);
+    }
+
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/UserServiceImpl.java
@@ -0,0 +1,69 @@
+package com.sky.service.impl;
+
+import com.sky.service.UserService;
+import com.sky.utils.HttpClientUtil;
+import com.alibaba.fastjson.JSONObject;
+
+import org.springframework.stereotype.Service;
+
+import com.alibaba.fastjson.JSON;
+import com.sky.constant.MessageConstant;
+import com.sky.dto.UserLoginDTO;
+import com.sky.entity.User;
+import com.sky.exception.LoginFailedException;
+import com.sky.properties.WeChatProperties;
+import com.sky.mapper.UserMapper;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    @Autowired
+    private WeChatProperties wechatProperties;
+    @Autowired
+    private UserMapper userMapper;
+
+    //微信服务接口地址
+    public static final String WX_LOGIN = "https://api.weixin.qq.com/sns/jscode2session";
+
+    @Override
+    public User wxLogin(UserLoginDTO userLoginDTO) {
+        //调用微信接口服务，获得当前微信用户的openid
+        Map<String, String> map = new HashMap<>();
+        map.put("appid",wechatProperties.getAppid());
+        map.put("secret",wechatProperties.getSecret());
+        map.put("js_code",userLoginDTO.getCode());
+        map.put("grant_type","authorization_code");
+        String json = HttpClientUtil.doGet(WX_LOGIN, map);
+
+        JSONObject jsonObject = JSON.parseObject(json);
+        String openid = jsonObject.getString("openid");
+
+        //判断openid是否为空
+        if(openid == null){
+            throw new LoginFailedException(MessageConstant.LOGIN_FAILED);
+        }
+
+        //判断当前用户是否为新用户
+        User user = userMapper.getByOpenid(openid);
+
+        //如果是新用户，完成注册
+        if(user == null){
+            user = User.builder()
+                    .openid(openid)
+                    .createTime(LocalDateTime.now())
+                    .build();
+            userMapper.insert(user);
+        }
+
+        
+        return user;
+    }
+
+}
